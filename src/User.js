ee9c3da096a2d3fdf7e180578cc3448ae0191482
Misaki Koonce <misaki.koonce@gmail.com>
2019-05-29 13:10:08 -0700

    Add game played condition.

--- src/User.js
+++ src/User.js
@@ -4,14 +4,24 @@ import PropTypes from 'prop-types';
 const User = props => {
     return (
         <li className="user">
+            <p>Username: {props.user.username}</p>
+            <p>Number of Games Played: {
+                props.user.gamePlayed 
+                ? (
+                    props.user.numGamePlayed
+                ) : (
+                    '0'
+                )
+            }
+            </p>
         </li>
     );
 };
 
 // Add PropTypes
 User.propTypes = {
-    user: PropTypes.array.isRequired,
-    numOfGamePlayed: PropTypes.string.isRequired,
+    user: PropTypes.object.isRequired,
+    gamePlayed: PropTypes.bool.isRequired,
 }
 
 export default User
