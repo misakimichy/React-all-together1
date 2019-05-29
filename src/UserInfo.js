06ef1414a7c009025754c396a65a9e04841c91ff
Misaki Koonce <misaki.koonce@gmail.com>
2019-05-29 11:59:26 -0700

    Add toggle button.

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class UserInfo extends Component {
+    state = {
+        numOfGamePlayed: '',
+    };
 
+    // Create toggle message
+    toggleGamePlayedMessage = () => {
+        this.state(prevState => ({
+            numOfGamePlayed: prevState,
+        }));
+    };
+    
+    render() {
+        const { numOfGamePlayed } = this.state;
+        const { users } = this.props;
+        return (
+            <div>
+                <h1>User</h1>
+                <button
+                    className="played-button"
+                    onClick={this.toggleGamePlayedMessage}
+                >
+                    {numOfGamePlayed ? 'Hide ' : 'Show '}
+                    The Number of Game Played
+                </button>
+            </div>
+        );
+    }
 }
+
+// Add PropTypes
+UserInfo.propTypes = {
+    users: PropTypes.array.isRequired,
+};
 
 export default UserInfo;
