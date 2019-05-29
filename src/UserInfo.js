e8fa98b4313a5d9985bccf340f75dc07a2cfe0f4
Misaki Koonce <misaki.koonce@gmail.com>
2019-05-29 13:10:49 -0700

    Change state to boolean from string.

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -4,42 +4,43 @@ import User from './User.js';
 
 class UserInfo extends Component {
     state = {
-        numOfGamePlayed: '',
+        gamePlayed: true,
     };
 
-    // Create toggle message
+    // Create toggle message and update state
     toggleGamePlayedMessage = () => {
-        this.state(prevState => ({
-            numOfGamePlayed: prevState,
+        this.setState(prevState => ({
+            gamePlayed: !prevState.gamePlayed,
         }));
     };
     
     render() {
-        const { numOfGamePlayed } = this.state;
+        const { gamePlayed } = this.state;
         const { users } = this.props;
         return (
             <div>
-                <h1>User</h1>
                 {users.length > 0 ? (
-                    <button
-                    className="played-button"
-                    onClick={this.toggleGamePlayedMessage}
-                >
-                    {numOfGamePlayed ? 'Hide ' : 'Show '}
-                    The Number of Game Played
-                </button>
-                ) : (
-                    ''
+                    <div>
+                        <h1>User</h1>
+                        <button
+                            className="played-button"
+                            onClick={this.toggleGamePlayedMessage}
+                        >
+                            {gamePlayed ? 'Hide ' : 'Show '}The Number of Game Played
+                        </button>
+                    </div>
+                    ) : (
+                        ''
                 )}
                 <ul>
                     {users.map(user => (
-                        <User key={user.username} user={user} numOfGamePlayed={numOfGamePlayed} />
+                        <User key={user.username} user={user} gamePlayed={gamePlayed} />
                     ))}
                 </ul>
             </div>
         );
     }
-}
+} 
 
 // Add PropTypes
 UserInfo.propTypes = {
